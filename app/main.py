3473676f16c38f4bf45ce612b5ff1c29be51bda7
GOavi101 <1704178@kiit.ac.in>
2024-04-28 05:54:47 +0530

    adding exit code 3

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import sys
 import os
 import shutil
 import tempfile
+import ctypes
 
 
 def main():
@@ -12,7 +13,10 @@ def main():
     dir_path = tempfile.mkdtemp()
     shutil.copy2(command, dir_path)
     os.chroot(dir_path)
-
+    
+    libc = ctypes.cdll.LoadLibrary("libc.so.6")
+    libc.unshare(0x20000000)
+    
     command = os.path.join("/", os.path.basename(command))
     completed_process = subprocess.run([command, *args], capture_output=True)
     sys.stdout.buffer.write(completed_process.stdout)
